60b3041b9cb6dc45fb958dfe6bf788195373adca
GOPII KRISSHNA M <133841523+GopiiKrisshna@users.noreply.github.com>
2025-04-17 14:56:56 +0530

    Add files via upload

---
+++ CICDPipeline/src/test/java/com/example/CICDPipeline/CicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CICDPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
